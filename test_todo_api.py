6466acacbe20231b9fe7375ac4eb931568582da6
ThanosPapas <thanpappas1@gmail.com>
2024-06-07 12:19:57 +0300

    first commit i guess

---
+++ test_todo_api.py
@@ -0,0 +1,26 @@
+import requests
+import pytest
+import pandas as pd
+
+
+
+url = 'https://todo.pixegami.io/'
+# response = requests.get(url)
+# print(response.headers['content-type'])
+# print(response.status_code)
+# j = response.json()
+# print(pd.json_normalize(j))
+
+def test_can_call_endpoint():               # need test_ to work
+    response = requests.get(url)
+    assert response.status_code == 200
+
+def test_can_create_task():
+    payload = {
+  "content": "test_c",
+  "user_id": "test_u",
+  "task_id": "test_t",
+  "is_done": False
+}
+    response = requests.put(url + 'create-task', json=payload)
+    assert response.status_code == 200
